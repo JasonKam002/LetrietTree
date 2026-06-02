b3b248790b463f37a0f8f862e0bece38e360ce64
JasonKam002 <jasonkam1311@gmail.com>
2026-06-02 11:29:50 -0300

    jun 2

--- script.js
+++ script.js
@@ -5,16 +5,33 @@ const inputName = document.querySelector(".form__input--name");
 const inputSpecies = document.querySelector(".form__input--species");
 const inputAge = document.querySelector(".form__input--age");
 const inputNumBird = document.querySelector(".form__input--numBird");
+const inputSearch = document.getElementById("#searchID");
 
 let map
 let mapEvent
+let logs = []
+
+//Leaflet marker icon
+var LeafIcon = L.Icon.extend({
+  options: {
+      iconSize:     [38, 50],
+      shadowSize:   [50, 64],
+      iconAnchor:   [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor:  [-3, -76]
+  }
+});
+
+var birdIcon = new LeafIcon({iconUrl: "bird.png"}),
+    treeIcon = new LeafIcon({iconUrl: "tree.png"});
+
 
 //objects
 class log {
   date = new Date();
   id = (Date.now() + "").slice(-10);
   category =  ''
-  constructor(coords, name, species, age) {}
+  constructor(coords, name, species) {}
 }
 
 class bird extends log{
@@ -58,7 +75,6 @@ navigator.geolocation.getCurrentPosition(
       
 
       map.on('click', function(mapE) {
-        form.style.display = 'block'
         mapEvent = mapE
         }
       )
@@ -68,47 +84,107 @@ navigator.geolocation.getCurrentPosition(
 
 // form event listener
 
-form.addEventListener('submit', function(e){
+/*form.addEventListener('submit', function(e){
   e.preventDefault()
 
   if (inputCategory.type == 'tree') {
     inputNumBird.visibility = 'hidden'
   }
   }
-)
+)*/
 
 // change type
 
 inputCategory.addEventListener('change', function(){
 
-  /*if (inputCategory.value == 'tree') {
+  if (inputCategory.value === 'tree') {
     inputNumBird.style.display = 'none'
     document.querySelector('.numBird').style.display = 'none'
     inputAge.style.display = 'block'
     document.querySelector('.age').style.display = 'block'
   }
 
-  if (inputCategory.value == 'bird') {
+  if (inputCategory.value === 'bird') {
     inputNumBird.style.display = 'block'
     document.querySelector('.numBird').style.display = 'block'
     inputAge.style.display = 'none'
     document.querySelector('.age').style.display = 'none'
-    console.log("1")
-  }*/
+  }
 
-  const numBirdRow = inputNumBird.closest(".form_row")
-  const ageRow = inputAge.closest(".form_row")
+  // const numBirdRow = inputNumBird.closest(".form_row")
+  // const ageRow = inputAge.closest(".form_row")
+
+  // if (inputCategory.value === 'tree') {
+  //   ageRow.classList.remove("form__row--hidden")
+  //   numBirdRow.classList.add("form__row--hidden")
+  //   console.log("1")
+  // }
+
+  // if (inputCategory.value === 'bird') {
+  //   ageRow.classList.add("form__row--hidden")
+  //   numBirdRow.classList.remove("form__row--hidden")
+  //   console.log("2")
+  // }
+
+}) 
+
+//submit btn
+
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+
+  const category = inputCategory.value
+  const name = inputName.value
+  const species = inputSpecies.value
+  const lng = mapEvent.latlng.lng
+  const lat = mapEvent.latlng.lat
+  let log 
 
   if (inputCategory.value === 'tree') {
-    ageRow.classList.remove("form__row--hidden")
-    numBirdRow.classList.add("form__row--hidden")
-    console.log("1")
+    const age = Number(inputAge.value)
+    log = new tree([lat, lng],name,species,age)
+
+    L.marker([lat, lng], {icon: treeIcon})
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "log-popup",
+      })
+    )
+    .setPopupContent(`Log by ${name} of ${category}`)
+    .openPopup();
   }
 
   if (inputCategory.value === 'bird') {
-    ageRow.classList.add("form__row--hidden")
-    numBirdRow.classList.remove("form__row--hidden")
-    console.log("2")
+    const numBird = Number(inputNumBird.value)
+    log = new bird([lat,lng],name,species,numBird)
+
+    L.marker([lat, lng], {icon: birdIcon})
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "log-popup",
+      })
+    )
+    .setPopupContent(`Log by ${name} of ${category}`)
+    .openPopup();
   }
 
-}) 
+  logs.push(log)
+
+  console.log(logs)
+})
+
+
+inputSearch.addEventListener("input", function(e) {
+
+  
+})
